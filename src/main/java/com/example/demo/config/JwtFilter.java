7f688f942a33054b92e1b94de93739bb4fd3cc6a
LucasFNS18 <lucasfns2040@gmail>
2025-02-28 18:29:37 -0300

    Primeiro COmmit

---
+++ src/main/java/com/example/demo/config/JwtFilter.java
@@ -0,0 +1,37 @@
+package com.example.demo.config;
+
+
+import com.example.demo.config.JwtUtil;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class JwtFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+            throws ServletException, IOException {
+
+        String header = request.getHeader("Authorization");
+
+        if (header != null && header.startsWith("Bearer ")) {
+            String token = header.substring(7);
+            if (JwtUtil.validateToken(token)) {
+                String username = JwtUtil.extractUsername(token);
+                UsernamePasswordAuthenticationFilter authentication =
+                        new UsernamePasswordAuthenticationFilter();
+                SecurityContextHolder.getContext().setAuthentication((Authentication) authentication);
+            }
+        }
+
+        chain.doFilter(request, response);
+    }
+}
---
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -0,0 +1,31 @@
+package com.example.demo.controller;
+
+
+import com.example.demo.dto.AuthDTO;
+import com.example.demo.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody AuthDTO request) {
+        return ResponseEntity.ok(authService.register(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody AuthDTO request) {
+        boolean authenticated = authService.authenticate(request);
+        if (authenticated) {
+            return ResponseEntity.ok("Login bem-sucedido!");
+        } else {
+            return ResponseEntity.status(401).body("Credenciais inválidas!");
+        }
+    }
+}
